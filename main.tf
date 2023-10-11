208f6894a1964e4617a1af30428040ac6ec874d5
Mn7376 <147593145+Mn7376@users.noreply.github.com>
2023-10-11 10:11:13 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    
+    region     = "us-east-1"
+    
+ 
+    
+}
+
+resource "aws_instance" "example" {
+   ami           = "ami-03a6eaae9938c858c"
+   instance_type = "t2.micro"
+
+tags {
+  Name = "New-Server"
+}}
+
+
+output "private_ip"{   
+     value = aws_instance.example.private_ip
+}
+output "public_ip"{
+     value = aws_instance.example.public_ip
+}
